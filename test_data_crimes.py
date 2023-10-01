8002d80df6e58ea249dcfad240dcd6e6ab51f0d8
Sofia Mase <84804699+sofiemase@users.noreply.github.com>
2023-10-01 14:46:43 -0700

    Add files via upload

---
+++ test_data_crimes.py
@@ -0,0 +1,11 @@
+#Sofia Mase
+# test_data_crimes.py
+
+# Import your code from data_crimes.py
+from data_crimes import my_function
+
+# Call a function from data_crimes.py
+result = my_function()
+
+# Print the result to verify
+print(result)
